9be9760ebdd07118410389060e6446e93a7127a8
kramuajay <kramuajay@outlook.com>
2023-05-23 07:46:44 -0400

    Null reference issues fix

--- Era-service-core/Helpers/AssistantDistanceComparer.cs
+++ Era-service-core/Helpers/AssistantDistanceComparer.cs
@@ -16,11 +16,15 @@ namespace Era_service_core.Helpers
             location = geolocation;
         }
 
-        public int Compare(Assistant a1, Assistant a2)
+        public int Compare(Assistant? a1, Assistant? a2)
         {
-            double distance1 = DistanceOperation.CalculateDistance(location, a1.GetLocation());
-            double distance2 = DistanceOperation.CalculateDistance(location, a2.GetLocation());
-            return distance1.CompareTo(distance2);
+            if (a1 != null && a2 != null)
+            {
+                double distance1 = DistanceOperation.CalculateDistance(location, a1.GetLocation());
+                double distance2 = DistanceOperation.CalculateDistance(location, a2.GetLocation());
+                return distance1.CompareTo(distance2);
+            }
+            return 0;
         }
 
        
